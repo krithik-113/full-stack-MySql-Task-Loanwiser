904e9fd4752c43ad07e9052c260d68aa64bfe55a
unknown <krithikroshan113@gmail.com>
2025-01-27 21:28:56 +0530

    add config file

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom'
 import DocumentsContext from './components/context API/DocumentsContext.jsx'
 import axios from 'axios'
 const root = createRoot(document.getElementById('root'))
-axios.defaults.baseURL = "http://localhost:3000"
+axios.defaults.baseURL = "https://back-end-mysql-task-loanwiser.onrender.com";
 root.render(
   <BrowserRouter>
     <DocumentsContext>
